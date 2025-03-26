e13d77add8d841ee7d256e8f7fa0f7f9316bf94a
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-03-26 20:23:39 +0000

    Exercicio 5 n e seed a funcionar

---
+++ RandDice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n =int.Parse(args[0]);
+            int s=int.Parse(args[1]);
+
+            Random dice=new Random(s);
+            
+
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine(dice.Next(1,6));
+                
+            }
+
+
+        }
+    }
+}
