fd141084ff661651dc4bc1c4f1e974ec938538ac
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-03-26 22:23:08 +0000

    Exercicio 10

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,22 @@ namespace MyGame
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int n=int.Parse(args[0]);
+            
+            Enemy[] inimigos= new Enemy[n];
+
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine($"Nome do inimigo{i+1}:");
+                string nome=Console.ReadLine();
+                inimigos[i]=new Enemy(nome);
+            }
+
+            foreach(Enemy i in inimigos)
+            {
+                Console.WriteLine($"{i.GetName()} {i.GetHealth()} {i.GetShield()}");
+            }
+
         }
     }
 }
