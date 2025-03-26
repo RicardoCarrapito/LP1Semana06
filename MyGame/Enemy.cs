9cf636d387604e8c5e360f9d00745b732d0c3ee1
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-03-26 21:39:09 +0000

    Exercicio 9

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -14,27 +14,55 @@ namespace MyGame
         //Construtor do enemy
         public Enemy(string name)
         {
-            this.name=name;
-            health=100;
-            shield=0;
+            SetName(name);
+            health = 100;
+            shield = 0;
         }
 
+        //devolve o name
         public string GetName()
         {
             return name;
         }
 
-
+        // dá damage ao enemy
         public void TakeDamage(float damage)
         {
-            shield-=damage;
-            if(shield<0)
+            shield -= damage;
+            if (shield < 0)
             {
-                float damageStillToInflict=-shield;
-                shield=0;
-                health-=damageStillToInflict;
-                if(health<0)health=0;
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
             }
         }
+
+        //devolve a health
+        public float GetHealth()
+        {
+            return health;
+        }
+
+        //devolve o shield
+        public float GetShield()
+        {
+            return shield;
+        }
+
+        public void SetName(string NewName)
+        {
+            if(NewName.Length>8)
+            {
+                name=NewName.Substring(0,8);
+            }
+            else
+            {
+                name=NewName;
+            }
+        }
+
+
+
     }
 }
